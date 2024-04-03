277f758722cc5454e9f8baff5a73dedfa6ecd706
jangkibong <82192866+jangkibong@users.noreply.github.com>
2024-04-03 17:10:32 +0900

    이미지데이터 png >>  jpeg 형식으로 변경, 데이터 압축 기능 추가

--- js/camera_backup.js
+++ js/camera_backup.js
@@ -2,18 +2,20 @@
 $(function(){
     const video = document.getElementById('video');
     const btn_camera_swiching = document.querySelector(".btn.camera_switch");
+    const btn_camera_flip = document.querySelector(".btn.camera_flip");
     const shutter_timer = $(".set_timer");
     const btn_shutter = $(".btn.shutter");
     const canvas = document.getElementById('canvas');
     const box_preview_item = $(".shootihg_box .picture li");
-
+    
     let stream;
     let currentDeviceIndex = 0;
     let photoCount = 0;
     let timer = shutter_timer.text(); // 기본 timer 10
     let shutter_status = false; //촬영중 : true, 촬영 정지 : false 
     let shutter_chance = window.localStorage.getItem("shutter chance"); // 8, 6, 4
-
+    let camera_flip_mode = true;
+    
     // 선택한 카메라 유지
     currentDeviceIndex = localStorage.getItem("current camera");
 
@@ -23,12 +25,13 @@ $(function(){
             const videoDevices = device.filter(device => device.kind === 'videoinput' && !device.label.toLowerCase().includes("virtual")); // 미디어장치 중 videoinupt 종류만 가져오기, 가상(virtual)카메라는 제외
             console.log('연결된 카메라');
             console.log(videoDevices);
+            localStorage.setItem("connecting video count", videoDevices.length); // 연결된 videoinput을 로컬스토리지에 저장
             // 연결된 카메라가 2개 이상일 때 카메라 전환 버튼 노출 
-            if(videoDevices.length > 1){
-                btn_camera_swiching.style.display = 'block';
-            }else{
-                btn_camera_swiching.style.display = 'none';
-            }
+            // if(videoDevices.length > 1){
+            //     btn_camera_swiching.style.display = 'block';
+            // }else{
+            //     btn_camera_swiching.style.display = 'none';
+            // }
             return navigator.mediaDevices.getUserMedia({video: {deviceId: videoDevices[currentDeviceIndex].deviceId}});
         })
         .then(function(newStream){
@@ -47,16 +50,36 @@ $(function(){
         });
     }
     
-    getCameraStream();
-    
-    btn_camera_swiching.addEventListener("click", function(){
-        const videoDevices = navigator.mediaDevices.enumerateDevices()
-            .then(function(device){
-                device.filter(device => device.kind === 'videoinput' && !device.label.toLowerCase().includes("virtual")); // 미디어장치 중 videoinupt 종류만 가져오기, 가상(virtual)카메라는 제외
-            })
+    // 카메라가 flip 모드일 때 비디오 화면 반전시키기
+    function flipCamera (){
+        if(camera_flip_mode == true){
+            video.style.transform = "scaleX(-1)"
+        } else{
+            video.style.transform = "scaleX(1)"
+        }
+    }
 
-        console.log("카메라 갯수: " + videoDevices)
-        currentDeviceIndex = (currentDeviceIndex + 1) % 2; // 2는 예시입니다. 실제로는 사용 가능한 카메라 수에 따라 달라집니다.
+    getCameraStream();
+    flipCamera();
+
+    btn_camera_flip.addEventListener('click', function(){
+        if(camera_flip_mode == true){
+            camera_flip_mode = false;
+        } else{
+            camera_flip_mode = true;
+        }
+        console.log(camera_flip_mode)
+        flipCamera();
+    });
+
+
+    btn_camera_swiching.addEventListener("click", function(){
+        let videoCount = localStorage.getItem("connecting video count"); // getCameraStream()에서 로컬스토리지에 저장한 카메라 수를 불러오기
+        if(currentDeviceIndex < videoCount - 1){
+            currentDeviceIndex ++
+        } else{
+            currentDeviceIndex = 0;
+        }
         localStorage.setItem("current camera", currentDeviceIndex);
         getCameraStream();
     });
@@ -77,8 +100,7 @@ $(function(){
                 timer --;
                 shutter_timer.text(timer)
                 
-                if(timer >= 0){
-                } else{
+                if(timer < 0){
                     shutter_timer.text(reset_timer); // 화면에 보이는 촬영 타이머 리셋
                     timer = shutter_timer.text();  //timer 변수를 리셋한 값으로 재할당
                     shutter_status = false;
@@ -86,10 +108,18 @@ $(function(){
                     console.log("남은 셔터 수", shutter_chance); //남은 셔터 기회 콘솔창에 보여주기
                     
                     // Canvas에 비디오 화면 그리기
-                    canvas.width = video.videoWidth;
-                    canvas.height = video.videoHeight;
-                    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-                    
+                    if(camera_flip_mode == true){
+                        const canvasContext = canvas.getContext('2d');
+                        canvas.width = video.videoWidth * 0.8; // 8장 이상 촬영 시 localstorage 용량초과로 인해 사이즈 줄임
+                        canvas.height = video.videoHeight * 0.8; // 8장 이상 촬영 시 localstorage 용량초과로 인해 사이즈 줄임
+                        canvasContext.scale(-1, 1); // 이미지를 수평으로 반전시킵니다
+                        canvasContext.drawImage(video, 0, 0, -canvas.width, canvas.height);
+                    } else{
+                        canvas.width = video.videoWidth * 0.8; // 8장 이상 촬영 시 localstorage 용량초과로 인해 사이즈 줄임
+                        canvas.height = video.videoHeight * 0.8; // 8장 이상 촬영 시 localstorage 용량초과로 인해 사이즈 줄임
+                        canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+                    }
+
                     // Canvas의 이미지를 Data URL로 변환하여 저장
                     const imgData = canvas.toDataURL('image/png');
                     
@@ -106,6 +136,8 @@ $(function(){
                     if(shutter_chance == 0){
                         clearInterval(interval_count); // 남은 셔터수가 0되면 카운트다운 종료 
                         $(".btn_ic.next").removeClass('hide'); // "다음"버튼 노출
+                        shutter_timer.text('');
+                        canvas.style.display = 'block';
                     }
                 }
             },1000);
